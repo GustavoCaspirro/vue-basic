d6f17f1993dcba6bb9b88aff5cdbebc975745857
FIAP <FIAP>
2019-11-12 22:29:49 -0300

    criando componente e usando v-for, v-bind, v-bind:key

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+});
+
 var app = new Vue({
     el: '#app',
     data: {
-        message: "Olá"
+        lista: [
+            {
+                id: 0,
+                text: "Tomate"
+            },
+            {
+                id: 1,
+                text: "Laranja"
+            },
+            {
+                id: 2,
+                text: "Bala"
+            }
+        ]
     }
 });
